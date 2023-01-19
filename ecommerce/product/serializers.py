eabfed533eafb33e2a735099bc9976293f329572
Karthik S <karthiks12335@gmail.com>
2023-01-19 13:26:35 +0530

    New models
    New models and Serializer and view

--- ecommerce/product/serializers.py
+++ ecommerce/product/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
-from .models import Brand,Category,Product
+from .models import Brand,Category,Product,ProductLine
 
 class CategorySerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source="name")
     class Meta:
         model = Category
-        fields = ['name']
+        fields = ['category_name']
 
 
 class BrandSerializer(serializers.ModelSerializer):
@@ -12,9 +13,17 @@ class BrandSerializer(serializers.ModelSerializer):
         model = Brand
         fields = ['name']
 
+class ProductLineSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ProductLine
+        exclude = ("id","product","is_active")
+
 class ProductSerializer(serializers.ModelSerializer):
-    brand = BrandSerializer()
-    category = CategorySerializer()
+    brand_name = serializers.CharField(source="brand.name")
+    category_name = serializers.CharField(source="category.name")
+    product_line = ProductLineSerializer(many=True)
     class Meta:
         model = Product
-        fields = "__all__"      
+        fields = ["name","description","brand_name","category_name","product_line",]    
+
--- ecommerce/tests/test_endpoint.py
+++ ecommerce/tests/test_endpoint.py
@@ -1,40 +1,41 @@
-import pytest
 import json
 
+import pytest
 
 pytestmark = pytest.mark.django_db
 
+
 class TestCategoryEndpoints:
-    
-    endpoint = '/api/category/'
+
+    endpoint = "/api/category/"
 
     def test_category_get(self, category_factory, api_client):
 
-        category_factory.create_batch(4)
+        category_factory.create_batch(0)
 
         respone = api_client().get(self.endpoint)
 
         assert respone.status_code == 200
-        #print(json.loads(respone.content))
-        assert len(json.loads(respone.content)) == 4
-
+        print(json.loads(respone.content))
+        assert len(json.loads(respone.content)) == 0
 
 
 class TestBrandEndpoints:
-    endpoint = '/api/brand/'
+    endpoint = "/api/brand/"
 
     def test_brand_get(self, brand_factory, api_client):
 
-        brand_factory.create_batch(4)
+        brand_factory.create_batch(0)
 
         respone = api_client().get(self.endpoint)
 
         assert respone.status_code == 200
-        #print(json.loads(respone.content))
-        assert len(json.loads(respone.content)) == 4
+        print(json.loads(respone.content))
+        assert len(json.loads(respone.content)) == 0
+
 
 class TestProductEndpoints:
-    endpoint = '/api/product/'
+    endpoint = "/api/product/"
 
     def test_product_get(self, product_factory, api_client):
 
@@ -45,4 +46,3 @@ class TestProductEndpoints:
         assert respone.status_code == 200
         print(json.loads(respone.content))
         assert len(json.loads(respone.content)) == 4
-
